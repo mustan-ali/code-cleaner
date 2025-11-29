3f6d0a824415e0a72b6d6ea68fe6f2edab72af01
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2025-11-29 09:21:01 +0500

    remove unused ANSI color codes from index.js

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ const depCheck = require("./src/depcheck");
 const importCheck = require("./src/importcheck");
 const utils = require("./src/utils");
 
-// ANSI color codes for better terminal output
-const colors = {
-    reset: "\x1b[0m",
-    bright: "\x1b[1m",
-    dim: "\x1b[2m",
-    cyan: "\x1b[36m",
-    green: "\x1b[32m",
-    yellow: "\x1b[33m",
-    red: "\x1b[31m",
-    magenta: "\x1b[35m",
-    blue: "\x1b[34m",
-    gray: "\x1b[90m"
-};
-
 function formatHeader(text) {
     return `\n${text}\n${"-".repeat(text.length)}\n`;
 }
